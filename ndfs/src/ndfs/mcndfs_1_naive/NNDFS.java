47f250f4da056fa3e8df767a053dbc4d4fc79293
iLikeCookiesQ <benniebees@hotmail.com>
2017-11-26 14:30:46 +0100

    new folder

---
+++ ndfs/src/ndfs/mcndfs_1_naive/NNDFS.java
@@ -0,0 +1,78 @@
+package ndfs.mcndfs_1_naive;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.lang.Thread;
+//import java.lang.Condition;
+import ndfs.NDFS;
+
+/**
+ * Implements the {@link ndfs.NDFS} interface, mostly delegating the work to a
+ * worker class.
+ */
+
+class MonitorObject{
+}
+
+class ThreadInfo{
+	public File pFile;
+	public int nWorker;
+	public boolean terminationResult;
+	public MonitorObject termination;
+	public boolean[] sense;
+	public int finishedCount;
+}
+
+public class NNDFS implements NDFS {
+    private Thread[] workers;
+    public ThreadInfo threadInfo;
+    /*
+    public volatile boolean[] terminationState = new boolean[1];
+    public Condition termination;*/
+
+    /**
+     * Constructs an NDFS object using the specified Promela file.
+     *
+     * @param promelaFile
+     *            the Promela file.
+     * @throws FileNotFoundException
+     *             is thrown in case the file could not be read.
+     */
+    public NNDFS(File promelaFile, int nrWorker) throws FileNotFoundException {
+	threadInfo.pFile = promelaFile;
+	threadInfo.nWorker = nrWorker;
+	threadInfo.terminationResult = false;
+	threadInfo.termination = new MonitorObject();
+	threadInfo.sense = new boolean[nrWorker]; // TODO: initialize these
+	threadInfo.finishedCount = 0;
+
+        workers = new Thread[nrWorker];
+        for(int i=0; i<threadInfo.nWorker; i++){
+            workers[i] = new Worker(threadInfo);
+        }
+
+        //this.worker = new Worker(promelaFile);
+    }
+
+    @Override
+    public boolean ndfs() {
+        for(int i = 0; i < threadinfo.nWorker; i++){
+	    // TODO put barrier inside threads to avoid cycles being found
+	    // before wait() is called here
+            workers[i].start();
+        }
+
+	synchronized(threadInfo.termination){
+		threadInfo.termination.wait();
+	}
+	if(threadInfo.terminationResult){
+	    //TODO terminate children
+	    for(int i = 0; i < threadInfo.nWorker; i++){
+		workers[i].interrupt();
+	    }
+	}
+	return threadInfo.terminationResult;
+	
+        //OLDCODE:return worker.getResult();
+    }
+}
---
+++ ndfs/src/ndfs/mcndfs_1_naive/Worker.java
@@ -0,0 +1,159 @@
+package ndfs.mcndfs_1_naive;
+import java.lang.Thread;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.concurrent.*;
+import java.util.HashMap;
+//import java.lang.Condition;
+
+import graph.Graph;
+import graph.GraphFactory;
+import graph.State;
+//import NNDFS.MonitorObject;
+//import NNDFS.ThreadInfo;
+
+/**
+ * This is a straightforward implementation of Figure 1 of
+ * <a href="http://www.cs.vu.nl/~tcs/cm/ndfs/laarman.pdf"> "the Laarman
+ * paper"</a>.
+ */
+//TODO: data structure for pink? no longer use the local red.
+public class Worker extends Thread {
+    private static HashMap<Graph.State, StateInfo> stateInfo;
+    private final Graph graph;
+    private final Colors colors = new Colors();
+    public ThreadInfo threadInfo;
+
+    // Throwing an exception is a convenient way to cut off the search in case a
+    // cycle is found.
+    private static class CycleFoundException extends Exception {
+    }
+
+    /**
+     * Constructs a Worker object using the specified Promela file.
+     *
+     *            the Promela file.
+     * @throws FileNotFoundException
+     *             is thrown in case the file could not be read.
+     */
+    public Worker(ThreadInfo threaddInfo) throws FileNotFoundException {
+	threadInfo = threaddInfo;
+	graph = GraphFactory.createGraph(threaddInfo.pFile);
+	stateInfo = new HashMap<Graph.State, StateInfo>();
+    }
+
+    private void dfsRed(Graph.State s) throws CycleFoundException {
+	if(Thread.interrupted()){
+		throw new InterruptedException();
+	}
+	colors.color(s, Color.RED); // make this node pink colors.color(t, Color.RED);
+        for (Graph.State t : graph.post(s)) {
+            if (colors.hasColor(t, Color.CYAN)) {
+                // signal main thread of cycle found
+		threadInfo.terminationResult = true;
+		synchronized(threadInfo.termination){
+			threadInfo.termination.notify();
+		}
+		return;
+                
+            } else if (!colors.hasColor(t, Color.RED)) { // this red is the pink from the paper
+                if(!stateInfo.get(t).red){ // this is the actual red
+			dfsRed(t);
+		}
+            }
+        }
+	if(s.isAccepting()){
+		StateInfo inf = stateInfo.get(s);
+		if(--inf.redCount == 0){
+			synchronized(inf){  // free all waiters
+				inf.notifyAll();
+			}
+		}
+		synchronized(inf){ // wait until redCount hits 0
+			inf.wait();
+		}
+	}
+	// TODO:actual pink false
+	// TODO:shared red true
+
+    }
+
+    private void dfsBlue(Graph.State s) throws CycleFoundException {
+	if(Thread.interrupted()){
+		throw new InterruptedException();
+	}
+        colors.color(s, Color.CYAN);
+
+	// randomly choose a child to begin recursive calls.
+	// access remainder of children in order.
+	int childCount = graph.post(s).size();
+	Graph.State[] children = new Graph.State[childCount];
+	int firstChildIdx = ThreadLocalRandom.current().nextInt(childCount);
+	for(int i = 0; i < childCount; i++){
+		int currentIdx = (firstChildIdx + i)%childCount; 
+		Graph.State currentChld = children[currentIdx];
+		if(colors.hasColor(currentChld, Color.WHITE)){
+			StateInfo inf = stateInfo.get(currentChld);
+			if(inf != null){
+				if(!inf.red) dfsBlue(currentChld);
+			} else {
+				stateInfo.put(currentChld, new StateInfo());
+				dfsBlue(currentChld);
+			}
+		}
+	}
+	
+        if (s.isAccepting()) {
+	    StateInfo inf = stateInfo.get(s);
+	    if(inf != null){
+		inf.redCount++;
+	    } else {
+		stateInfo.put(s, new StateInfo(false, 1));
+	    }
+            dfsRed(s);
+            colors.color(s, Color.RED);
+        } else {
+            colors.color(s, Color.BLUE);
+        }
+    }
+
+    private void nndfs(Graph.State s) throws CycleFoundException {
+        dfsBlue(s);
+	
+	//signal main thread that last worker has finished
+	if(getAndIncrement(threadInfo.finishedCount) == threadInfo.nWorker -1){
+		threadInfo.terminationResult = false;
+		synchronized(threadInfo.termination){
+			threadInfo.termination.notify();
+		}
+	}
+    }
+    @Override
+    public void run() {
+        nndfs(graph.getInitialState());
+    }
+
+
+
+    // returns random index 0 < x < nChildren
+    int getRandomIndex(int nChildren){
+	
+    }
+    /*OLDCODE:public boolean getResult() {
+        return result;
+    }*/
+}
+
+class StateInfo{
+	boolean red;
+	int redCount;
+
+	StateInfo(){
+		red = false;
+		redCount = 0;
+	}
+	StateInfo(boolean b, int i){
+		red = b;
+		redCount = i;
+	}
+}
