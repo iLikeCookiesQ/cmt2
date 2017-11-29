17ec31bc3193573ca7d507570ed7ebc3a8d87b4b
iLikeCookiesQ <benniebees@hotmail.com>
2017-11-29 14:27:17 +0100

    some debug print statements

--- ndfs/src/ndfs/mcndfs_1_naive/Worker.java
+++ ndfs/src/ndfs/mcndfs_1_naive/Worker.java
@@ -20,7 +20,7 @@ import graph.GraphFactory;
  */
 //TODO: data structure for pink? no longer use the local red.
 public class Worker implements Runnable {
-	static final boolean DEBUG = true;
+	static final boolean DEBUG = false;
 	String threadName;
 	private static HashMap<State, StateInfo> stateInfo;
 	private HashSet<State> pink;
